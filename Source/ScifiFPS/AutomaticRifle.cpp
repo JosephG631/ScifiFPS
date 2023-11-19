6421a71f2e94335c11b55eeba100de0f14547b80
Joseph Gilmore <79208860+JosephG631@users.noreply.github.com>
2023-11-19 20:44:10 +0000

    Update AutomaticRifle.cpp

--- Source/ScifiFPS/AutomaticRifle.cpp
+++ Source/ScifiFPS/AutomaticRifle.cpp
@@ -5,5 +5,6 @@
 
 AAutomaticRifle::AAutomaticRifle()
 {
+	// What are these random magic floats ? should this not be done in a intiliszer list?
 	ADSLocation = FVector(-16.000000f, 1.500000f, -166.950000f);
 }
