ae9107bca0b6855a8479782e86053835998687aa
Joseph Gilmore <79208860+JosephG631@users.noreply.github.com>
2023-11-19 20:41:45 +0000

    Update AmmoCounterWidget.cpp

--- Source/ScifiFPS/AmmoCounterWidget.cpp
+++ Source/ScifiFPS/AmmoCounterWidget.cpp
@@ -1,26 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+// Get rid of this unless you are going to fill it ^
 #include "AmmoCounterWidget.h"
 #include "Kismet/KismetTextLibrary.h"
 
 void UAmmoCounterWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
-
+	// You getting several pointers here, you need to check each one for nullness 
 	AScifiFPSCharacter* player = Cast<AScifiFPSCharacter>(GetWorld()->GetFirstPlayerController()->GetCharacter());
 	if(player)
 	{
+		// I might not be understanding the ue code here but is Player a memeber var? not clean enough
 		Player = player;
 	}
 }
 
 FText UAmmoCounterWidget::GetAmmoText() const
 {
+	// You getting several pointers here, you need to check each one for nullness 
 	return UKismetTextLibrary::Conv_IntToText(Player->GetWeaponComponent()->GetCurrentAmmoOfCurrentWeapon());
 }
 
 FText UAmmoCounterWidget::GetTotalAmmoText() const
 {
+	// You getting several pointers here, you need to check each one for nullness 
 	return UKismetTextLibrary::Conv_IntToText(Player->GetWeaponComponent()->GetReserveAmmoOfCurrentWeapon());
 }
