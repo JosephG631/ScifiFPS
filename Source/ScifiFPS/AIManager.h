27c7e74ee17d71053ec71735f306ca86f553b94c
Joseph Gilmore <79208860+JosephG631@users.noreply.github.com>
2023-11-19 20:26:55 +0000

    Update AIManager.h

--- Source/ScifiFPS/AIManager.h
+++ Source/ScifiFPS/AIManager.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+// Make sure includes are alphabetical 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AIManager.generated.h"
@@ -12,18 +13,20 @@ class SCIFIFPS_API AAIManager : public ACharacter
 	GENERATED_BODY()
 
 public:
+// I don't know if this is ue5 naming convention but the name AAIManger is not clear. is that actor AI manager? 
 	// Sets default values for this character's properties
 	AAIManager();
 
 protected:
+// Does this need to be protected if you are overriding it in child?
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+	// Called every frame // comment not needed
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
+	// Called to bind functionality to input // comment not needed 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
